8d87d8262bf9c891533788d14dedad2e823d49ff
thedeadc0der <jessy.vanderaugstraete@gmail.com>
2019-06-01 15:28:56 +0200

    Nettoyage d'éléments non utilisés

--- app/src/main/java/com/example/android_chat/ChoixConvActivity.java
+++ app/src/main/java/com/example/android_chat/ChoixConvActivity.java
@@ -6,24 +6,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.android_chat.api.ApiController;
 import com.example.android_chat.model.Conversation;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.List;
 
--- app/src/main/java/com/example/android_chat/customListener.java
+++
@@ -1,5 +0,0 @@
-package com.example.android_chat;
-
-public interface customListener<T> {
-    public void getResult(T object);
-}
