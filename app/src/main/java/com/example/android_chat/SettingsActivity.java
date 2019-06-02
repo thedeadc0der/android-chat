9cb5097a2afe8b861f53d47f254961b15ceb3042
thedeadc0der <jessy.vanderaugstraete@gmail.com>
2019-06-02 14:24:29 +0200

    Nettoyage d'éléments abandonnés

--- app/src/main/java/com/example/android_chat/SettingsActivity.java
+++ app/src/main/java/com/example/android_chat/SettingsActivity.java
@@ -4,16 +4,9 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
-
-    // A déclarer dans le MANIFESTE !
-    // Cf. <activity android:name=".SettingsActivity"></activity>
-    // dans la balise <application>
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO: Utiliser des fragments plutot qu'une activité 'préférences'
         //noinspection deprecation
         addPreferencesFromResource(R.xml.preferences);
     }
