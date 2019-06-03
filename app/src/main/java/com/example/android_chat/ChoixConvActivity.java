7c17be4b098a8a4b0fca9d42bc161c226b738266
thedeadc0der <jessy.vanderaugstraete@gmail.com>
2019-06-03 23:55:52 +0200

    Isolation du ConversationListAdapter

--- app/src/main/java/com/example/android_chat/ChoixConvActivity.java
+++ app/src/main/java/com/example/android_chat/ChoixConvActivity.java
@@ -1,55 +1,18 @@
 package com.example.android_chat;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.android_chat.api.ApiController;
 import com.example.android_chat.model.Conversation;
 
 import java.util.List;
-
-class ConversationListAdapter extends ArrayAdapter {
-    private List<Conversation> conversations;
-    private Activity activity;
-    
-    public ConversationListAdapter(Activity context, List<Conversation> conversations){
-        super(context, R.layout.conversation_item, conversations);
-        this.activity = context;
-        this.conversations = conversations;
-    }
-    
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        Conversation conversation = conversations.get(position);
-        
-        LayoutInflater inflater = activity.getLayoutInflater();
-        View rowView = inflater.inflate(R.layout.conversation_item, null, true);
-        
-        TextView convTitle = rowView.findViewById(R.id.conv_lblTitle);
-        convTitle.setText(conversation.getTheme());
-        
-        TextView convLastMsg = rowView.findViewById(R.id.conv_lblLastMsg);
-        convLastMsg.setText(conversation.getLastMessage());
-        
-        View convColor = rowView.findViewById(R.id.conv_color);
-        convColor.setBackgroundColor(conversation.isActive()
-		        ? activity.getResources().getColor(R.color.active_conversation)
-		        : activity.getResources().getColor(R.color.inactive_conversation));
-        
-        return rowView;
-    }
-}
 
 public class ChoixConvActivity extends CommonActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener, View.OnClickListener {
     private List<Conversation> conversations;
---
+++ app/src/main/java/com/example/android_chat/ConversationListAdapter.java
@@ -0,0 +1,44 @@
+package com.example.android_chat;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.android_chat.model.Conversation;
+
+import java.util.List;
+
+public class ConversationListAdapter extends ArrayAdapter {
+	private List<Conversation> conversations;
+	private Activity activity;
+	
+	public ConversationListAdapter(Activity context, List<Conversation> conversations){
+		super(context, R.layout.conversation_item, conversations);
+		this.activity = context;
+		this.conversations = conversations;
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent){
+		Conversation conversation = conversations.get(position);
+		
+		LayoutInflater inflater = activity.getLayoutInflater();
+		View rowView = inflater.inflate(R.layout.conversation_item, null, true);
+		
+		TextView convTitle = rowView.findViewById(R.id.conv_lblTitle);
+		convTitle.setText(conversation.getTheme());
+		
+		TextView convLastMsg = rowView.findViewById(R.id.conv_lblLastMsg);
+		convLastMsg.setText(conversation.getLastMessage());
+		
+		View convColor = rowView.findViewById(R.id.conv_color);
+		convColor.setBackgroundColor(conversation.isActive()
+				? activity.getResources().getColor(R.color.active_conversation)
+				: activity.getResources().getColor(R.color.inactive_conversation));
+		
+		return rowView;
+	}
+}
