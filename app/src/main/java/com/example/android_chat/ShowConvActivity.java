57a5df28f4d2ac8430424b7f76d403cca94f8e2f
thedeadc0der <jessy.vanderaugstraete@gmail.com>
2019-06-03 13:46:09 +0200

    FIX: Ne pas récupérer immédiatement les messages

--- app/src/main/java/com/example/android_chat/ShowConvActivity.java
+++ app/src/main/java/com/example/android_chat/ShowConvActivity.java
@@ -154,7 +154,7 @@ public class ShowConvActivity extends CommonActivity implements View.OnClickList
                     }
                 });
             }
-        }, 0, 1000 * 5);
+        }, 1000 * 5, 1000 * 5);
     }
     
     @Override
